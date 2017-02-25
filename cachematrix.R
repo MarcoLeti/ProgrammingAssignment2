f86c72ede9567133f2c2460ad954075eb04994ca
MarcoLeti <letico.marco@gmail.com>
2017-02-25 18:21:50 +0100

    adding modified file cachematrix.R for Assignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+## the code below calculates the inverse of a matrix and stores
+## its value on the cache. Until the variables doesn't change
+## the value of the inverse of the matrix is retrieved from the cache.
+## If not it is recalculated.
 
-## Write a short comment describing this function
+## the function solve() calculates the inverse of a matrix
+## if we put just the first argument "a", see ?solve for details
+
+## the function "makeCacheMatrix" is a list of 4 functions:
+## a the begin the variable x and i are inizialized as "empty/null"
+## the first function sets the value of the matrix
+## the second gets the value of the matrix
+## the third sets the value of the inverse of the matrix
+## the fourth gets the value of the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        i <- NULL
+        setMatrix <- function(y) {
+                x <<- y
+                i <<- NULL
+        }
+        getMatrix <- function() x
+        setMatrixInv <- function(inverse) i <<- inverse
+        getMatrixInv <- function() i
+        list(setMatrix = setMatrix, getMatrix = getMatrix,
+             setMatrixInv = setMatrixInv,
+             getMatrixInv = getMatrixInv)
 }
 
 
-## Write a short comment describing this function
+## the function "cacheSolve" retrieves the value of the matrix from the cache
+## or calculates the inverse of the matrix if the value it's not present on the cache.
+## in the first part there is an if test to check if the value is on the cache
+## if not the second part calculates it and stores it on the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        i <- x$getMatrixInv()
+        if(!is.null(i)) {
+                message("getting cached data")
+                return(i)
+        }
+        data <- x$getMatrix()
+        i <- solve(data, ...)
+        x$setMatrixInv(i)
+        i
 }
